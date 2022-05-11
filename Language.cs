df0ab2c6f8730a796388639c4e5b2a8286f6e51f
yingran <yingran@live.cn>
2022-05-11 23:45:07 +0800

    整理项目

---
+++ Language.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotfix.FishingCF
+{
+	public class Language
+	{
+		public const string DaSanYuan = "大三元";
+		public const string DaSiXi = "大四喜";
+		public const string SondDeng = "送灯";
+		public const string SanDian = "闪电";
+		public const string CaiJing = "彩金";
+		public const string TotalTurn = "总局数";
+		public const string FinshingCF = "超凡捕鱼";
+	}
+}
---
+++ MyApp.cs
@@ -0,0 +1,32 @@
+﻿using Hotfix.Common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Hotfix.Common;
+using UnityEngine.SceneManagement;
+
+namespace Hotfix.FishingCF
+{
+	public class MyApp : AppBase
+	{
+		public ThisGameConfig conf = new ThisGameConfig();
+		public GameController theGame = new GameController();
+ 
+		public override void Start()
+		{
+			game = theGame;
+			conf.Init();
+			base.Start();
+
+			Debug.Log("进入热更！！！");
+			//加载主场景
+			//MyApp.LoadAssets<>
+			//SceneManager.LoadScene();
+
+		}
+	}
+}
